8f87a929e4556d1a7758685319e569f8e51952bd
zavod008 <zavod1357@mail.ru>
2021-11-21 15:08:08 +0300

    fix start function result

--- android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
+++ android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
@@ -8,27 +8,30 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import androidx.annotation.Nullable;
+
+import androidx.activity.result.ActivityResult;
+
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
+import com.getcapacitor.Logger;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.annotation.Permission;
-import com.orhanobut.logger.AndroidLogAdapter;
-import com.orhanobut.logger.BuildConfig;
-import com.orhanobut.logger.Logger;
+
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.locks.ReentrantLock;
-import org.json.JSONArray;
 
 @CapacitorPlugin(
   permissions = {
     @Permission(
-      strings = { Manifest.permission.RECORD_AUDIO },
+      strings = {Manifest.permission.RECORD_AUDIO},
       alias = "record_audio"
     ),
   }
@@ -48,16 +51,6 @@ public class SpeechRecognition extends Plugin implements Constants {
   public void load() {
     super.load();
 
-    Logger.addLogAdapter(
-      new AndroidLogAdapter() {
-
-        @Override
-        public boolean isLoggable(int priority, @Nullable String tag) {
-          return BuildConfig.DEBUG;
-        }
-      }
-    );
-
     bridge
       .getWebView()
       .post(
@@ -70,7 +63,7 @@ public class SpeechRecognition extends Plugin implements Constants {
             SpeechRecognitionListener listener = new SpeechRecognitionListener();
             speechRecognizer.setRecognitionListener(listener);
 
-            Logger.i("Instantiated SpeechRecognizer in load()");
+            Logger.debug("Instantiated SpeechRecognizer in load()");
           }
         }
       );
@@ -78,7 +71,7 @@ public class SpeechRecognition extends Plugin implements Constants {
 
   @PluginMethod
   public void available(PluginCall call) {
-    Logger.i("Called for available(): %b", isSpeechRecognitionAvailable());
+    Logger.info("Called for available(): %b", isSpeechRecognitionAvailable() ? "yes" : "no");
     boolean val = isSpeechRecognitionAvailable();
     JSObject result = new JSObject();
     result.put("available", val);
@@ -153,7 +146,7 @@ public class SpeechRecognition extends Plugin implements Constants {
   public void hasPermission(PluginCall call) {
     call.resolve(
       new JSObject()
-      .put("permission", hasAudioPermissions(RECORD_AUDIO_PERMISSION))
+        .put("permission", hasAudioPermissions(RECORD_AUDIO_PERMISSION))
     );
   }
 
@@ -164,48 +157,11 @@ public class SpeechRecognition extends Plugin implements Constants {
         bridge
           .getActivity()
           .requestPermissions(
-            new String[] { RECORD_AUDIO_PERMISSION },
+            new String[]{RECORD_AUDIO_PERMISSION},
             REQUEST_CODE_PERMISSION
           );
-        call.resolve();
-      } else {
-        call.resolve();
       }
-    }
-  }
-
-  @Override
-  protected void handleOnActivityResult(
-    int requestCode,
-    int resultCode,
-    Intent data
-  ) {
-    super.handleOnActivityResult(requestCode, resultCode, data);
-
-    PluginCall savedCall = getSavedCall();
-    if (savedCall == null) {
-      return;
-    }
-
-    if (requestCode == REQUEST_CODE_SPEECH) {
-      if (resultCode == Activity.RESULT_OK) {
-        try {
-          ArrayList<String> matchesList = data.getStringArrayListExtra(
-            RecognizerIntent.EXTRA_RESULTS
-          );
-          JSObject result = new JSObject();
-          result.put("matches", new JSArray(matchesList));
-          savedCall.resolve(result);
-        } catch (Exception ex) {
-          savedCall.reject(ex.getMessage());
-        }
-      } else {
-        savedCall.reject(Integer.toString(requestCode));
-      }
-
-      SpeechRecognition.this.lock.lock();
-      SpeechRecognition.this.listening(false);
-      SpeechRecognition.this.lock.unlock();
+      call.resolve();
     }
   }
 
@@ -233,7 +189,7 @@ public class SpeechRecognition extends Plugin implements Constants {
     boolean showPopup,
     PluginCall call
   ) {
-    Logger.i("Beginning to listen for audible speech");
+    Logger.debug("Beginning to listen for audible speech");
 
     final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
     intent.putExtra(
@@ -254,8 +210,8 @@ public class SpeechRecognition extends Plugin implements Constants {
     }
 
     if (showPopup) {
-      saveCall(call);
-      startActivityForResult(call, intent, REQUEST_CODE_SPEECH);
+      bridge.saveCall(call);
+      startActivityForResult(call, intent, "processListening");
     } else {
       bridge
         .getWebView()
@@ -307,6 +263,34 @@ public class SpeechRecognition extends Plugin implements Constants {
       );
   }
 
+  @ActivityCallback
+  public void processListening(PluginCall call, ActivityResult result) {
+    if (call == null) {
+      return;
+    }
+
+    ArrayList<String> matchesList;
+
+    Intent data = result.getData();
+
+    if (result.getResultCode() == Activity.RESULT_OK) {
+      try {
+        matchesList = data.getStringArrayListExtra(
+          RecognizerIntent.EXTRA_RESULTS
+        );
+        JSObject object = new JSObject();
+        object.put("matches", new JSArray(matchesList));
+        call.resolve(object);
+      } catch (Exception ex) {
+        call.reject(ex.getMessage());
+      }
+    }
+
+    SpeechRecognition.this.lock.lock();
+    SpeechRecognition.this.listening(false);
+    SpeechRecognition.this.lock.unlock();
+  }
+
   private class SpeechRecognitionListener implements RecognitionListener {
     private PluginCall call;
 
@@ -315,27 +299,32 @@ public class SpeechRecognition extends Plugin implements Constants {
     }
 
     @Override
-    public void onReadyForSpeech(Bundle params) {}
+    public void onReadyForSpeech(Bundle params) {
+    }
 
     @Override
-    public void onBeginningOfSpeech() {}
+    public void onBeginningOfSpeech() {
+    }
 
     @Override
-    public void onRmsChanged(float rmsdB) {}
+    public void onRmsChanged(float rmsdB) {
+    }
 
     @Override
-    public void onBufferReceived(byte[] buffer) {}
+    public void onBufferReceived(byte[] buffer) {
+    }
 
     @Override
-    public void onEndOfSpeech() {}
+    public void onEndOfSpeech() {
+    }
 
     @Override
     public void onError(int error) {
       SpeechRecognition.this.stopListening();
-      String errorMssg = getErrorText(error);
+      String errorMsg = getErrorText(error);
 
       if (this.call != null) {
-        call.reject(errorMssg);
+        call.reject(errorMsg);
       }
     }
 
@@ -350,15 +339,15 @@ public class SpeechRecognition extends Plugin implements Constants {
 
         if (this.call != null) {
           this.call.resolve(
-              new JSObject().put("status", "success").put("matches", jsArray)
-            );
+            new JSObject().put("status", "success").put("matches", jsArray)
+          );
         }
       } catch (Exception ex) {
         this.call.resolve(
-            new JSObject()
-              .put("status", "error")
-              .put("message", ex.getMessage())
-          );
+          new JSObject()
+            .put("status", "error")
+            .put("message", ex.getMessage())
+        );
       }
     }
 
@@ -372,16 +361,18 @@ public class SpeechRecognition extends Plugin implements Constants {
       try {
         if (
           matches != null &&
-          matches.size() > 0 &&
-          !previousPartialResults.equals(matchesJSON)
+            matches.size() > 0 &&
+            !previousPartialResults.equals(matchesJSON)
         ) {
           previousPartialResults = matchesJSON;
         }
-      } catch (Exception ex) {}
+      } catch (Exception ex) {
+      }
     }
 
     @Override
-    public void onEvent(int eventType, Bundle params) {}
+    public void onEvent(int eventType, Bundle params) {
+    }
   }
 
   private String getErrorText(int errorCode) {
